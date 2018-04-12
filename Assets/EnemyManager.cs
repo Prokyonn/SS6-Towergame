f1a5024767df94a23e8682f2bda72fac2e4ce374
Michelle <michelle.weber@students.fhv.at>
2018-04-12 20:51:22 +0200

    added enemy manager and walk script

---
+++ Assets/EnemyManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject Enemy;
+
+    private GameObject spawnedEnemy;
+
+    public float speed = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("spawn", 2f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    private void spawn()
+    {
+        Vector3 origin = Enemy.transform.position;
+        SpawnEnemy(new Vector3(Random.Range(origin.x - 1, origin.x + 1), origin.y, origin.z));
+
+    }
+
+    private void SpawnEnemy(Vector3 position)
+    {
+        var enemy = GameObject.FindGameObjectWithTag("Orc").transform;
+        spawnedEnemy = Instantiate(Enemy, position, enemy.rotation) as GameObject;
+    }
+
+    private void OnCollisionEnter(Collision colision)
+    {
+        if (colision.gameObject.tag == "Orc")
+        {
+            gameObject.GetComponent<Animation>().Play("Attack");
+        }
+    }
+
+
+}
